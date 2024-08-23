79549e3c924ebbd7cc8853dc989c6092df173fb1
timredd <timredd13@gmail.com>
2024-08-23 10:56:12 -0400

    Get sidebar and map laid out correctly

--- src/components/map/basic-example.tsx
+++ src/components/map/basic-example.tsx
@@ -1,7 +1,8 @@
-import { CustomMap, CustomMapRoot } from "@/features/map/create-map-context";
+import { CustomMap, MapProvider } from "@/features/map/create-map-context";
 import { ViewportInfo } from "@/features/map/viewport-info";
+import { cn } from "@/lib/utils";
 import "maplibre-gl/dist/maplibre-gl.css";
-import { type ComponentProps, splitProps } from "solid-js";
+import { type ComponentProps, createSignal, splitProps } from "solid-js";
 import { Sidebar } from "./sidebar";
 
 export interface MapProps extends ComponentProps<"div"> {}
@@ -9,14 +10,22 @@ export interface MapProps extends ComponentProps<"div"> {}
 export const ExampleMap = (props: MapProps) => {
   const [, rest] = splitProps(props, ["class"]);
 
+  const [isCollapsed, setIsCollapsed] = createSignal(false);
+
   return (
-    <>
-      <Sidebar>
-        <CustomMapRoot>
-          <CustomMap />
-          <ViewportInfo />
-        </CustomMapRoot>
-      </Sidebar>
-    </>
+    <div class="flex flex-row">
+      <MapProvider>
+        <Sidebar
+          isCollapsed={isCollapsed()}
+          setIsCollapsed={setIsCollapsed}
+          class={cn(
+            "z-10 bg-background",
+            isCollapsed() ? "w-[200px]" : "w-[396px]",
+          )}
+        />
+        <CustomMap />
+        <ViewportInfo />
+      </MapProvider>
+    </div>
   );
 };
--- src/components/map/sidebar.tsx
+++ src/components/map/sidebar.tsx
@@ -1,140 +1,77 @@
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Resizable,
-  ResizableHandle,
-  ResizablePanel,
-} from "@/components/ui/resizable";
-import { Separator } from "@/components/ui/separator";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { usePlaceOfInterest } from "@/features/map/constants";
+import { useMap } from "@/features/map/create-map-context";
+import type { MaybeAccessor } from "@/lib/solid";
 import { cn } from "@/lib/utils";
-import type { ContextValue } from "@corvu/resizable";
-import { type ParentProps, createSignal } from "solid-js";
-import { For, type JSX, Show } from "solid-js";
+import { ArrowLeftToLineIcon, ArrowRightToLineIcon } from "lucide-solid";
+import {
+  type Accessor,
+  type ParentProps,
+  Show,
+  createEffect,
+  splitProps,
+} from "solid-js";
+import { reconcile } from "solid-js/store";
 
-export const Sidebar = (props: ParentProps) => {
-  const [sizes, setSizes] = createSignal([]);
-  const [isCollapsed, setIsCollapsed] = createSignal(true);
-  const [resizableContext, setResizableContext] =
-    createSignal<ContextValue | null>(null);
+export interface SidebarProps extends ParentProps {
+  class?: string;
+  isCollapsed?: boolean;
+  setIsCollapsed?: (isCollapsed: boolean) => void;
+}
 
-  return (
-    <div class="hidden md:block">
-      <Resizable
-        class="border-r bg-background"
-        sizes={sizes()}
-        onSizesChange={setSizes}
-      >
-        <ResizablePanel
-          // initialSize={0.2}
-          // initialSize={sizes()[0] ?? 0.2}
-          minSize={0.2}
-          // maxSize={0.3}
-          // collapsible
-          // onCollapse={(e) => setIsCollapsed(e === 0)}
-          // onExpand={() => setIsCollapsed(false)}
-          // class={cn(
-          //   isCollapsed() &&
-          //     "min-w-[50px] transition-all duration-300 ease-in-out",
-          // )}
-        >
-          <Separator />
-          <div class="container flex flex-col items-center gap-10 py-10">
-            <h1 class="font-semibold text-2xl">Mask Bans</h1>
-            <div class="grid">
-              <Button variant="outline">Nassau County</Button>
-            </div>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel>{props.children}</ResizablePanel>
-      </Resizable>
-    </div>
-  );
-};
+export const Sidebar = (props: SidebarProps) => {
+  const [, rest] = splitProps(props, ["class"]);
 
-type Props = {
-  isCollapsed: boolean;
-  links: {
-    title: string;
-    label?: string;
-    icon: JSX.Element;
-    variant: "default" | "ghost";
-  }[];
-};
+  const map = useMap();
 
-export const Nav = (props: Props) => {
+  const nassau = usePlaceOfInterest("nassau");
+
+  createEffect(() => {
+    if (map.viewport().inTransit) {
+      console.log("in transit");
+    }
+  });
+
   return (
     <div
-      data-collapsed={props.isCollapsed}
-      class="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
+      class={cn(
+        "container flex flex-col items-center gap-10 py-16",
+        props.class,
+      )}
+      {...rest}
     >
-      <nav class="grid gap-1 px-2 group-data-[collapsed=true]:justify-center group-data-[collapsed=true]:px-2">
-        <For each={props.links}>
-          {(item) => (
-            <Show
-              when={props.isCollapsed}
-              fallback={
-                <a
-                  // biome-ignore lint/a11y/useValidAnchor: <explanation>
-                  href="#"
-                  class={cn(
-                    buttonVariants({
-                      variant: item.variant,
-                      size: "sm",
-                      class: "text-sm",
-                    }),
-                    item.variant === "default" &&
-                      "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-                    "justify-start",
-                  )}
-                >
-                  <div class="mr-2">{item.icon}</div>
-                  {item.title}
-                  {item.label && (
-                    <span
-                      class={cn(
-                        "ml-auto",
-                        item.variant === "default" &&
-                          "text-background dark:text-white",
-                      )}
-                    >
-                      {item.label}
-                    </span>
-                  )}
-                </a>
-              }
-            >
-              <Tooltip openDelay={0} closeDelay={0} placement="right">
-                <TooltipTrigger
-                  as="a"
-                  href="#"
-                  class={cn(
-                    buttonVariants({ variant: item.variant, size: "icon" }),
-                    "h-9 w-9",
-                    item.variant === "default" &&
-                      "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white",
-                  )}
-                >
-                  {item.icon}
-                  <span class="sr-only">{item.title}</span>
-                </TooltipTrigger>
-                <TooltipContent class="flex items-center gap-4">
-                  {item.title}
-                  <Show when={item.label}>
-                    <span class="ml-auto text-muted-foreground">
-                      {item.label}
-                    </span>
-                  </Show>
-                </TooltipContent>
-              </Tooltip>
-            </Show>
-          )}
-        </For>
-      </nav>
+      <Button
+        variant="outline"
+        size="icon"
+        class="absolute top-1 right-1"
+        onClick={() => props.setIsCollapsed?.(!props.isCollapsed)}
+      >
+        <Show
+          when={props.isCollapsed}
+          fallback={<ArrowLeftToLineIcon class="size-4" />}
+        >
+          <ArrowRightToLineIcon class="size-4" />
+        </Show>
+      </Button>
+      <h1 class="font-semibold text-2xl">Mask Bans</h1>
+      <div class="grid">
+        <Button
+          variant="outline"
+          onClick={() => {
+            console.log("Clicked");
+            map.setViewport({
+              center: nassau.coords,
+              zoom: nassau.zoom,
+              pitch: 225,
+            });
+          }}
+        >
+          Nassau County
+        </Button>
+        <Show when={!map.viewport().inTransit}>
+          <p>We've landed in Nassau</p>
+        </Show>
+      </div>
     </div>
   );
 };

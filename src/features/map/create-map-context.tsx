edc5dd4a346528b556b16717a31a806d3ab8e51a
timredd <timredd13@gmail.com>
2024-08-23 11:52:29 -0400

    Mess around with ux

--- src/features/map/create-map-context.tsx
+++ src/features/map/create-map-context.tsx
@@ -1,11 +1,15 @@
 import { cn } from "@/lib/utils";
 import * as maplibre from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { MapPinIcon, PinIcon } from "lucide-solid";
 import {
   type ComponentProps,
   For,
   createContext,
+  createEffect,
+  createMemo,
   createSignal,
+  onMount,
   splitProps,
   useContext,
 } from "solid-js";
@@ -20,6 +24,7 @@ import {
 } from "solid-map-gl";
 import {
   type PlaceOfInterest,
+  type PlaceOfInterestKey,
   placesOfInterest,
   usePlaceOfInterest,
 } from "./constants";
@@ -27,6 +32,7 @@ import {
 export type MapState = {
   viewport: Viewport;
   poi: PlaceOfInterest[];
+  currentLocation: PlaceOfInterestKey | null;
 };
 
 const createMapStore = () => {
@@ -38,6 +44,7 @@ const createMapStore = () => {
       center: usa.coords,
       zoom: usa.zoom,
     } as Viewport,
+    currentLocation: null,
   } as const);
 };
 
@@ -47,17 +54,52 @@ const makeMapContext = () => {
   const [mapContext] = useSolidMapContext();
 
   const [store, setStore] = createMapStore();
+
+  // const viewport = createMemo(() => store.viewport);
+  // const setViewport = (vp: Viewport) => {
+  //   setStore("viewport", vp);
+  // };
+
   const usa = usePlaceOfInterest("usa");
   const [viewport, setViewport] = createSignal<Viewport>({
     center: usa.coords,
     zoom: usa.zoom,
-  } as Viewport);
+  });
 
-  // const viewport = createMemo(() => store.viewport);
-  //
-  // const setViewport = (viewport: Viewport) => {
-  //   setStore("viewport", viewport);
+  // const currentLocation = createMemo(() => store.currentLocation);
+  // const setCurrentLocation = (placeOfInterest: PlaceOfInterestKey) => {
+  //   setStore("currentLocation", placeOfInterest);
   // };
+
+  // createEffect(
+  //   on(currentLocation, (location) => {
+  //     if (location === null) {
+  //       resetViewport();
+  //     }
+  //     if (location !== null) {
+  //       flyTo(location);
+  //     }
+  //   }),
+  // );
+
+  const resetViewport = () => {
+    const usa = usePlaceOfInterest("usa");
+    setViewport(usa);
+  };
+
+  const flyTo = (placeOfInterest: PlaceOfInterestKey, options?: Viewport) => {
+    const poi = usePlaceOfInterest(placeOfInterest);
+    if (!poi) {
+      throw new Error(`Couldn't find ${placeOfInterest}`);
+    }
+    console.log(`Flying to ${JSON.stringify(poi)}`);
+    const where = {
+      center: poi.coords,
+      zoom: poi.zoom,
+      ...options,
+    };
+    setViewport(where);
+  };
 
   return {
     ref: mapRef,
@@ -66,6 +108,9 @@ const makeMapContext = () => {
     setStore: setStore,
     viewport: viewport,
     setViewport: setViewport,
+    // currentLocation: currentLocation,
+    // setCurrentLocation: setCurrentLocation,
+    flyTo: flyTo,
   };
 };
 
@@ -100,11 +145,25 @@ export const CustomMap = (props: CustomMapProps) => {
 
   const customMap = useMap();
 
+  // TODO: hide the map controls
+  onMount(() => {
+    const ctrl = document.querySelector("div.maplibregl-ctrl-bottom-right");
+
+    if (ctrl) {
+      ctrl.setAttribute("style", "display: none");
+      return;
+    }
+    console.log("no ctrl");
+  });
+
   return (
     <SolidMapGL
       mapLib={maplibre}
       viewport={customMap.viewport()}
-      onViewportChange={(viewport: Viewport) => customMap.setViewport(viewport)}
+      onViewportChange={(viewport: Viewport) => {
+        console.log("setting viewport", JSON.stringify(viewport));
+        customMap.setViewport(viewport);
+      }}
       ref={customMap.ref}
       transitionType="flyTo"
       class={cn("h-dvh w-full", props.class)}
@@ -115,18 +174,20 @@ export const CustomMap = (props: CustomMapProps) => {
       {...rest}
     >
       <Source
+        id="counties"
         source={{
           type: "geojson",
           data: "http://localhost:4321/api/counties.geojson",
-          filter: ["all", ["==", "NAME", "Nassau"]],
+          // filter: ["all", ["==", "NAME", "Nassau"]],
         }}
       >
         <Layer
+          visible
+          sourceId="counties"
           style={{
-            type: "fill",
+            type: "line",
             paint: {
-              color: "#999",
-              "outline-color": "#000",
+              color: "#e399ee",
             },
           }}
         />
@@ -135,8 +196,14 @@ export const CustomMap = (props: CustomMapProps) => {
         {(poi) => (
           <Marker
             showPopup={false}
-            options={{ color: "#333FFF" }}
+            options={{
+              color: "#333FFF",
+              // element: <MapPinIcon class="size-10 text-red-800" />,
+            }}
             lngLat={poi.coords}
+            on:click={(evt: MouseEvent) => {
+              console.log("CLICKED WOOHOO", evt);
+            }}
           >
             {poi.name}
           </Marker>

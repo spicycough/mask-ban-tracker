bca59b2b73c49055bec8c4ca89b328f4327285dd
timredd <timredd13@gmail.com>
2024-09-06 11:00:36 -0400

    Add selectable column components

---
+++ src/pages/index/components/select-column-def.tsx
@@ -0,0 +1,57 @@
+import { Checkbox, CheckboxControl } from "@/components/ui/checkbox";
+import type { Location } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import type {
+  CellContext,
+  HeaderContext,
+  Table as SolidTable,
+} from "@tanstack/solid-table";
+import { type ComponentProps, splitProps } from "solid-js";
+
+export const SelectedRowsHeaderCheckbox = <TData, TValue>(
+  props: HeaderContext<TData, TValue>,
+) => {
+  return (
+    <Checkbox
+      indeterminate={props.table.getIsSomePageRowsSelected()}
+      checked={props.table.getIsAllPageRowsSelected()}
+      onChange={(value) => props.table.toggleAllPageRowsSelected(!!value)}
+      aria-label="Select all"
+    >
+      <CheckboxControl />
+    </Checkbox>
+  );
+};
+
+export const SelectedRowsCellCheckbox = <TData, TValue>(
+  props: CellContext<TData, TValue>,
+) => {
+  return (
+    <Checkbox
+      checked={props.row.getIsSelected()}
+      onChange={(value) => props.row.toggleSelected(!!value)}
+      aria-label="Select row"
+    >
+      <CheckboxControl />
+    </Checkbox>
+  );
+};
+
+export const SelectedRowsText = <TData,>(
+  props: ComponentProps<"div"> & {
+    class?: string;
+    table: SolidTable<TData>;
+  },
+) => {
+  const [local, rest] = splitProps(props, ["class"]);
+
+  return (
+    <div
+      class={cn("flex-1 text-muted-foreground text-sm", local.class)}
+      {...rest}
+    >
+      {props.table.getFilteredSelectedRowModel().rows.length} of{" "}
+      {props.table.getFilteredRowModel().rows.length} row(s) selected.
+    </div>
+  );
+};

36b03ce3fa80f6a075995a49332c3be62cf6cdf6
timredd <timredd13@gmail.com>
2024-09-10 11:14:20 -0400

    Update page

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,129 +1,82 @@
 import { Button } from "@/components/ui/button";
-import { hc } from "@/lib/hono";
-import { QueryBoundary } from "@/lib/vike-solid-query";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { useMapContext } from "@/stores/map";
-import { createQuery } from "@tanstack/solid-query";
-import { center as getCenter } from "@turf/center";
 import { ArrowUpRightIcon } from "lucide-solid";
-import { For } from "solid-js";
-import { LocationList, LocationListItem } from "./components/location-list";
+import { For, onMount } from "solid-js";
+import { LocationList } from "./components/location-list";
 import { CustomMap } from "./components/map";
 import { ViewportInfo } from "./components/viewport-info";
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import type { Location } from "@/db/schema";
-import { LocationDataTable } from "./components/location-data-table";
-import statesData from "./components/us-states.geojson";
-
-// This type is used to define the shape of our data.
-// You can use a Zod or Validbot schema here if you want.
-function getStates() {
-  return statesData.features.map((feature) => ({
-    name: feature.properties.name,
-  }));
-}
+import statesData from "@/constants/us-states.geojson";
 
 export default function Page() {
-  const { state, flyTo } = useMapContext();
+  const { map, flyTo, resetViewport, currentLocation, setCurrentLocation } =
+    useMapContext();
 
-  const query = createQuery(() => ({
-    queryKey: ["map", "locations"],
-    queryFn: getLocations,
-  }));
-
-  const countiesQuery = createQuery(() => ({
-    queryKey: ["map", "counties"],
-    queryFn: getCounties,
-  }));
+  onMount(() => {
+    resetViewport();
+  });
 
   return (
-    <QueryBoundary
-      query={query}
-      loadingFallback={<div class="">Loading...</div>}
-    >
-      {(data) => (
-        <div class="flex h-screen w-full bg-gray-100 dark:bg-gray-800">
-          <div class="flex flex-1">
-            <div class="flex-1 space-y-4 p-4">
-              <div class="flex items-center justify-end">
-                <Button variant="outline" class="text-gray-500">
-                  <ArrowUpRightIcon class="mr-2 h-4 w-4" />
-                  Share
-                </Button>
-              </div>
-
-              <CustomMap
-                locations={data}
-                class="flex h-1/2 items-center justify-center rounded-lg border border-border bg-gray-200 text-gray-400 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300"
-              />
-
-              {/* <div class="flex-1 space-y-4 bg-blue-400 p-4"> */}
-              {/*   <h1 class="font-bold text-2xl">View locations</h1> */}
-              {/*   <LocationDataTable data={() => data} /> */}
-              {/* </div> */}
-            </div>
-
-            <LocationList>
-              <div class="space-y-4">
-                <For each={statesData.features}>
-                  {(feature) => (
-                    <Card
-                      onClick={() => {
-                        const center = () => getCenter(feature);
-                        flyTo({
-                          pitch: 225,
-                          zoom: 4,
-                          center: {
-                            lat: center().geometry.coordinates[1],
-                            lon: center().geometry.coordinates[0],
-                          },
-                        });
-                      }}
-                    >
-                      <CardHeader>
-                        <CardTitle>{feature.properties.name}</CardTitle>
-                      </CardHeader>
-                      <CardContent>
-                        <p class="text-sm">Test</p>
-                      </CardContent>
-                      <CardFooter>
-                        <Button variant="outline" class="text-gray-500">
-                          <ArrowUpRightIcon class="mr-2 h-4 w-4" />
-                        </Button>
-                      </CardFooter>
-                    </Card>
-                  )}
-                </For>
-              </div>
-            </LocationList>
+    <div class="flex h-screen w-full bg-gray-100 dark:bg-gray-800">
+      <div class="flex flex-1">
+        <div class="flex-1 space-y-4 p-4">
+          <div class="flex items-center justify-between">
+            <h1 class="font-bold text-2xl">{currentLocation()?.name}</h1>
+            <Button variant="outline" class="text-gray-500">
+              <ArrowUpRightIcon class="mr-2 h-4 w-4" />
+              Share
+            </Button>
           </div>
-          <ViewportInfo />
+
+          <CustomMap class="flex h-1/2 items-center justify-center rounded-lg border border-border bg-gray-200 text-gray-400 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300" />
+
+          {/* <div class="flex-1 space-y-4 bg-blue-400 p-4"> */}
+          {/*   <h1 class="font-bold text-2xl">View locations</h1> */}
+          {/*   <LocationDataTable data={() => data} /> */}
+          {/* </div> */}
         </div>
-      )}
-    </QueryBoundary>
+
+        <LocationList>
+          <div class="space-y-2">
+            <For each={statesData.features}>
+              {(feature) => {
+                // const center = turfCenterOfMass(feature);
+                // const bbox = feature.bbox?.at(0);
+                return (
+                  <Card
+                    class="aria-selected:bg-blue-300"
+                    onClick={() => {
+                      setCurrentLocation({
+                        kind: "state",
+                        name: feature.properties?.name,
+                      });
+                      // flyTo({
+                      //   pitch: 225,
+                      //   center: center.geometry.coordinates,
+                      // });
+                    }}
+                  >
+                    <CardHeader class="py-3">
+                      <span class="flex w-full items-center justify-between">
+                        <CardTitle>{feature.properties?.name}</CardTitle>
+                        <Button
+                          variant="ghost"
+                          size="icon"
+                          class="text-gray-500"
+                        >
+                          <ArrowUpRightIcon class="h-4 w-4" />
+                        </Button>
+                      </span>
+                    </CardHeader>
+                  </Card>
+                );
+              }}
+            </For>
+          </div>
+        </LocationList>
+      </div>
+      <ViewportInfo />
+    </div>
   );
 }
-
-const getLocations = async () => {
-  const resp = await hc.map.locations.$get();
-  if (!resp.ok) {
-    const { status, statusText } = resp;
-    throw new Error(`${status} ${statusText}`);
-  }
-  return (await resp.json()) as Location[];
-};
-
-const getCounties = async () => {
-  const resp = await hc.map.locations.$get();
-  if (!resp.ok) {
-    const { status, statusText } = resp;
-    throw new Error(`${status} ${statusText}`);
-  }
-  return (await resp.json()) as Location[];
-};

f1b9aed63e8f05fb28ca381994e84ae37296fe53
timredd <timredd13@gmail.com>
2024-09-17 12:23:22 -0400

    Update admin edit page

--- src/pages/admin/@id/+Page.tsx
+++ src/pages/admin/@id/+Page.tsx
@@ -1,34 +1,19 @@
-import { FormSubmitButton, FormTextField } from "@/components/ui/form";
-import { hc } from "@/lib/hono";
+import { useBanDetails } from "@/api/bans/queries";
+import {
+  FormSelect,
+  FormSubmitButton,
+  FormTextField,
+} from "@/components/ui/form";
+import { RegionType } from "@/db/schema";
 import { QueryBoundary } from "@/lib/vike-solid-query";
 import { createForm } from "@modular-forms/solid";
-import { createQuery } from "@tanstack/solid-query";
 import { createEffect } from "solid-js";
 import { usePageContext } from "vike-solid/usePageContext";
 
 export default function Page() {
-  // const queryClient = useQueryClient();
-  // const hydratedData = queryClient.getQueryData<ResponseType>(["bans", banId]);
-
   const banId = usePageContext().routeParams.id;
 
-  const query = createQuery(() => ({
-    queryKey: ["bans", banId],
-    queryFn: async () => {
-      const res = await hc.bans[":id"].$get({
-        param: { id: banId },
-      });
-
-      if (res.status === 200) {
-        return res.json();
-      }
-      throw new Error(res.statusText);
-    },
-  }));
-
-  createEffect(() => {
-    console.log(JSON.stringify(hydratedData, null, 2));
-  });
+  const query = useBanDetails(banId);
 
   return (
     <QueryBoundary query={query}>
@@ -37,41 +22,106 @@ export default function Page() {
           initialValues: data,
         });
 
+        createEffect(() => {
+          console.log(data);
+        });
+
         return (
-          <div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-            <div class="flex items-center justify-between space-y-2">
-              <div>
-                <h2 class="font-bold text-2xl tracking-tight">
-                  {data?.regionId}
-                </h2>
-                <p class="text-muted-foreground">
-                  View, create, edit, and delete ban.
-                </p>
+          <div class="h-full p-8">
+            <div class="space-y-8 rounded-lg border border-border bg-secondary p-10">
+              <div class="flex items-center justify-between space-y-2">
+                <div>
+                  <h2 class="text-muted-foreground">
+                    View, create, edit, and delete ban.
+                  </h2>
+                  <h1 class="font-bold text-2xl tracking-tight">
+                    {data?.regionName}
+                  </h1>
+                </div>
               </div>
+              <EditBan.Form class="grid grid-flow-row grid-cols-2 gap-4">
+                <div class="items-center justify-between space-y-2">
+                  <h3 class="font-semibold text-lg">General</h3>
+                  <EditBan.Field name="regionType">
+                    {(field, props) => (
+                      <FormSelect
+                        {...props}
+                        label="Region type"
+                        options={Object.entries(RegionType).map(
+                          ([label, value]) => ({ label, value }),
+                        )}
+                        placeholder="Enter a region name..."
+                        value={field.value}
+                        error={field.error}
+                        required
+                      />
+                    )}
+                  </EditBan.Field>
+                  <EditBan.Field name="regionName" type="string">
+                    {(field, props) => (
+                      <FormTextField
+                        {...props}
+                        label="Region"
+                        placeholder="Enter a region name..."
+                        value={field.value}
+                        error={field.error}
+                        required
+                      />
+                    )}
+                  </EditBan.Field>
+                  <EditBan.Field name="condition" type="string">
+                    {(field, props) => (
+                      <FormTextField
+                        {...props}
+                        label="Condition"
+                        placeholder="Enter a condition..."
+                        value={field.value}
+                        error={field.error}
+                        required
+                      />
+                    )}
+                  </EditBan.Field>
+                </div>
+
+                <div class="items-center justify-between space-y-2">
+                  <h3 class="font-semibold text-lg">Penalties</h3>
+                  <EditBan.Field name="fineMin" type="number">
+                    {(field, props) => (
+                      <FormTextField
+                        {...props}
+                        label="Fine (minimum)"
+                        placeholder="Enter a dollar amount..."
+                        value={field.value}
+                        error={field.error}
+                        required
+                      />
+                    )}
+                  </EditBan.Field>
+                  <EditBan.Field name="fineMax" type="number">
+                    {(field, props) => (
+                      <FormTextField
+                        {...props}
+                        label="Fine (maximum)"
+                        placeholder="Enter a dollar amount..."
+                        value={field.value}
+                        error={field.error}
+                        required
+                      />
+                    )}
+                  </EditBan.Field>
+                </div>
+                <div class="col-span-2">
+                  <FormSubmitButton
+                    type="submit"
+                    form={form}
+                    error="Please correct errors."
+                    variant="default"
+                  >
+                    Save
+                  </FormSubmitButton>
+                </div>
+              </EditBan.Form>
             </div>
-            <EditBan.Form class="w-1/2 space-y-2">
-              <EditBan.Field name="condition" type="string">
-                {(field, props) => (
-                  <FormTextField
-                    {...props}
-                    type="text"
-                    label="Condition"
-                    placeholder="jane@example.com"
-                    value={field.value}
-                    error={field.error}
-                    required
-                  />
-                )}
-              </EditBan.Field>
-              <FormSubmitButton
-                type="submit"
-                form={form}
-                error="Please correct errors."
-                variant="default"
-              >
-                Save
-              </FormSubmitButton>
-            </EditBan.Form>
           </div>
         );
       }}

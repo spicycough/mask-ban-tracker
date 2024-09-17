26c2680d10f19bd2763b6abf8501e33551fb9421
timredd <timredd13@gmail.com>
2024-09-17 12:22:56 -0400

    Add toTitlecase util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 export function getTitle(title = "Home") {
   return "%s | Vike Solid".replace("%s", title);
 }
+
+export function toTitlecase(str: string) {
+  return str.replace(/\w\S*/g, (s) => {
+    const [first, rest] = [s.charAt(0), s.slice(1)];
+    return first.toUpperCase() + rest.toLowerCase();
+  });
+}

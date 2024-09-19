44a7ad06d80e4f42226983034d4c7a2d789634e0
timredd <timredd13@gmail.com>
2024-09-19 10:43:42 -0400

    Update 404 page

--- src/pages/_error/+Page.tsx
+++ src/pages/_error/+Page.tsx
@@ -1,23 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-solid";
 import { Show } from "solid-js";
 import { usePageContext } from "vike-solid/usePageContext";
 
 export default function Page() {
-  const { is404, ...content } = usePageContext();
+  const { is404 } = usePageContext();
 
   return (
-    <Show
-      when={is404}
-      fallback={
-        <>
-          <h1>500 Internal Server Error</h1>
-          <p>Something went wrong. Boop</p>
-          <p>{`${content.data}`}</p>
-        </>
-      }
-    >
-      <h1>404 Page Not Found</h1>
-      <p>This page could not be found.</p>
-      <p>{`${content.data}`}</p>
-    </Show>
+    <div class="flex min-h-screen flex-col items-center justify-center bg-background text-foreground">
+      <main class="space-y-6 p-4 text-center">
+        <Show
+          when={is404}
+          fallback={
+            <Status
+              header="500"
+              subheader="Internal server error"
+              content="Something went wrong. Boop"
+            />
+          }
+        >
+          <Status
+            header="404"
+            subheader="Page not found"
+            content="The page you are looking for might have been removed, had its name changed, or is temporarily unavailable."
+          />
+        </Show>
+        <Button as="a" href="/" variant="default" class="mt-4">
+          <ArrowLeftIcon class="mr-2 h-4 w-4" />
+          Go to home
+        </Button>
+      </main>
+    </div>
   );
 }
+
+type StatusProps = {
+  header: string;
+  subheader?: string;
+  content: string;
+};
+
+const Status = (props: StatusProps) => {
+  const { data } = usePageContext();
+
+  return (
+    <>
+      <h1 class="font-bold text-4xl sm:text-6xl">{props.header}</h1>
+      <p class="text-xl sm:text-2xl">{props.subheader}</p>
+      <p class="mx-auto max-w-md text-muted-foreground">{props.content}</p>
+      <Show when={data}>
+        <p class="mx-auto max-w-md text-muted-foreground">{`${data}`}</p>
+      </Show>
+    </>
+  );
+};

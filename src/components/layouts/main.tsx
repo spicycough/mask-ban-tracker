78ddf724f308694ecff7595c496d0d032db3e6b6
timredd <timredd13@gmail.com>
2024-09-19 11:57:20 -0400

    Clean up layouts

---
+++ src/components/layouts/main.tsx
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <main class="flex-1">
+      <div class="mx-auto max-w-5xl px-12">
+        <div class="prose w-full max-w-none pt-6 pb-24">Test</div>
+      </div>
+      <h1>v0 Changelog</h1>
+      <p>All the latest updates, improvements, and fixes to v0.</p>
+    </main>
+  );
+};
--- src/components/layouts/vertical-layout.tsx
+++ src/components/layouts/vertical-layout.tsx
@@ -5,20 +5,54 @@ import {
   type ValidComponent,
   splitProps,
 } from "solid-js";
-import VerticalFooter from "./vertical-footer";
-import VerticalNav from "./vertical-nav";
+import { FloatingNavbar } from "../floating-navbar";
 
-export type VerticalLayoutProps<T extends ValidComponent = "div"> =
-  ComponentProps<T>;
+export type VerticalLayoutProps<T extends ValidComponent = "div"> = FlowProps<
+  ComponentProps<T> & {
+    class?: string;
+  }
+>;
 
-export default function VerticalLayout(props: FlowProps<VerticalLayoutProps>) {
-  const [local, rest] = splitProps(props as VerticalLayoutProps, ["class"]);
+export const Root = <T extends ValidComponent = "div">(
+  props: VerticalLayoutProps<T>,
+) => {
+  const [local, rest] = splitProps(props as VerticalLayoutProps<T>, [
+    "class",
+    "children",
+  ]);
 
   return (
-    <div class={cn("flex min-h-screen flex-col", local.class)} {...rest}>
-      <VerticalNav />
+    <div
+      id="app"
+      class={cn("flex min-h-dvh flex-col bg-neutral-200", local.class)}
+      {...rest}
+    >
+      <FloatingNavbar />
       <main class="relative flex-1">{props.children}</main>
-      <VerticalFooter />
+      <footer>Mask Ban Tracker</footer>
     </div>
   );
-}
+};
+
+export const FullWidth = <T extends ValidComponent = "div">(
+  props: VerticalLayoutProps<T>,
+) => {
+  return <>{props.children}</>;
+};
+
+export const NarrowWidth = <T extends ValidComponent = "div">(
+  props: VerticalLayoutProps<T>,
+) => {
+  const [local, rest] = splitProps(props as VerticalLayoutProps<T>, [
+    "class",
+    "children",
+  ]);
+
+  return (
+    <div class={cn("mx-auto max-w-5xl px-12", local.class)} {...rest}>
+      <div class="h-screen bg-neutral-100 dark:bg-gray-800">
+        {props.children}
+      </div>
+    </div>
+  );
+};
